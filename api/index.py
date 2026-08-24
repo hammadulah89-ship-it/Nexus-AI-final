02d19c8a67dc09a32defb02a23ffff0e0e07d158
hammadulah89-ship-it <hammadulah89@gmail.com>
2026-08-24 15:42:00 +0000

    Fix: Serverless build routes and static resolution for Vercel

--- api/index.py
+++ api/index.py
@@ -3,7 +3,9 @@ import sys
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
-if parent_dir not in sys.path:
-    sys.path.insert(0, parent_dir)
+
+for path in [parent_dir, current_dir, os.getcwd()]:
+    if path and path not in sys.path:
+        sys.path.insert(0, path)
 
 from main import app
